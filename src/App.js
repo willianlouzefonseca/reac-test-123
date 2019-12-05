2c33b9f755bd9a540b5d3bec2740ef30d8b3b175
willianlouzefonseca <williamlouzefonseca333@gmail.com>
2019-12-05 13:38:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Chip from './components/chip/chip';
+import Card from './components/card/card';
+import Dropdown from './components/dropdown/dropdown';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      car: [
+        {
+          id: 0,
+          title: 'Monza',
+          selected: false,
+          key: 'car'
+        },
+        {
+          id: 1,
+          title: 'Civic',
+          selected: false,
+          key: 'car'
+        },
+        {
+          id: 2,
+          title: 'Corolla',
+          selected: false,
+          key: 'car'
+        },
+        {
+          id: 3,
+          title: 'City',
+          selected: false,
+          key: 'car'
+        },
+        {
+          id: 4,
+          title: 'Fit',
+          selected: false,
+          key: 'car'
+        }
+      ]
+    }
+    this.toggleSelected = this.toggleSelected.bind(this);
+    this.resetThenSet = this.resetThenSet.bind(this)
+  }
+
+  toggleSelected = (id, key) => {
+    let temp = JSON.parse(JSON.stringify(this.state[key]))
+    temp[id].selected = !temp[id].selected
+    this.setState({
+      [key]: temp
+    })
+  }
+
+  resetThenSet = (id, key) => {
+    let temp = JSON.parse(JSON.stringify(this.state[key]))
+    temp.forEach(item => item.selected = false);
+    temp[id].selected = true;
+    this.setState({
+      [key]: temp
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <div className='col'>
+          <Chip avatar={'https://picsum.photos/id/237/200/200'}/>
+        </div>
+        <div className='col'>
+          <Card image={'https://picsum.photos/id/237/200/300'}/>
+        </div>
+        <div className='col'>
+          <Dropdown 
+            title="Select a car"
+            list={this.state.car}
+            resetThenSet={this.resetThenSet}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Ballon/Ballon.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './styles.css';
+
+
+class Ballon extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+
+        console.log(this.props.avatar)
+    }
+
+    render(){
+        return(
+            <div className="chip">
+                <img src={this.props.avatar} width="200" alt="teste" height="200"/>
+            </div>
+        )       
+    };
+}
+
+export default Ballon;
